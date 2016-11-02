d4f7dba5139297341f42fb8ae79ba4fd50272a77
itderrickh <heined50@uwosh.edu>
2016-11-02 09:13:41 -0500

    Half working website.

--- wwwroot/js/loginModule.js
+++ wwwroot/js/loginModule.js
@@ -1,14 +1,30 @@
-app.controller('LoginController', ['$scope', function($scope) {
+app.service('LoginService', ['$http', 'AuthenticationToken', function($http, AuthenticationToken) {
+    var LOGIN_URL = "http://www.itderrickh.com/users/login/"
+    return {
+        login: function(credentials, callback) {
+            $http.post(LOGIN_URL, credentials).then(function(result) {
+                AuthenticationToken.setToken(result.data);
+                callback();
+            }, function(result) {
+                console.log(result);
+            });
+        }
+    }
+}]);
+
+app.controller('LoginController', ['$scope', '$location', 'LoginService', function($scope, $location, LoginService) {
     var loginCtrl = this;
     loginForm.email = "";
     loginCtrl.password = "";
 
     loginCtrl.submit = function() {
         var credentials = {
-            "email": loginForm.email,
+            "email": loginCtrl.email,
             "password": loginCtrl.password
         };
 
-        alert(credentials.email);
+        LoginService.login(credentials, function() {
+            $location.path("main");
+        });
     };
 }]);
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,49 @@
 var app = angular.module('noteCloud', ['ngRoute']);
+
+app.service('APIInterceptor', ['$rootScope', 'AuthenticationToken', '$location', function($rootScope, AuthenticationToken, $location) {
+    var service = this;
+    service.request = function(config) {
+        var access_token = AuthenticationToken.getToken();
+        if (access_token != null && access_token != "") {
+            config.headers.Authorize = access_token;
+        }
+
+        return config;
+    };
+    service.responseError = function(response) {
+        if (response.status === 401) {
+            $rootScope.$broadcast('unauthorized');
+        }
+        return response;
+    };
+}])
+
+app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "/views/login.html",
+        controller: "LoginController",
+        controllerAs: "loginCtrl"
+    })
+    .when('/main', {
+        templateUrl: "/views/main.html",
+        controller: "MainController",
+        controllerAs: "mainCtrl"
+    });
+
+    $httpProvider.interceptors.push('APIInterceptor');
+}]);
+
+app.factory('AuthenticationToken', ['$window', function($window) {
+    return {
+        getToken: function() {
+            return $window.localStorage['authToken'];
+        },
+        setToken: function(token) {
+            $window.localStorage['authToken'] = token;
+        },
+        destroyToken: function() {
+            $window.localStorage['authToken'] = null;
+        }
+    };
+}]);
